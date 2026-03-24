c3a167e3c26946099a254e9c6eec938d02fba0c2
IvanRL25 <146905269+IvanRL25@users.noreply.github.com>
2026-03-23 23:54:45 -0500

    Artifact CRUD done! Close #1

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/converter/WizardtoWizardDtoConverter.java
@@ -0,0 +1,21 @@
+package edu.tcu.cs.hogwartsartifactsonline.wizard.converter;
+
+import edu.tcu.cs.hogwartsartifactsonline.wizard.dto.WizardDto;
+import edu.tcu.cs.hogwartsartifactsonline.wizard.Wizard;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class WizardtoWizardDtoConverter implements Converter<Wizard, WizardDto> {
+
+    @Override
+    public WizardDto convert(Wizard source){
+        WizardDto wizardDto = new WizardDto(source.getId(),
+                source.getName(),
+                source.getNumberOfArtifacts());
+        return wizardDto;
+    }
+
+}
